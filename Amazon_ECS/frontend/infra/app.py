b8320d050b1d9874abd81cc7d52b1bc423875f7c
Cobus Bernard <cobusb@amazon.com>
2021-03-11 22:11:40 +0200

    base platOform... :facepalm:

--- Amazon_ECS/frontend/infra/app.py
+++ Amazon_ECS/frontend/infra/app.py
@@ -71,7 +71,7 @@ class FrontendService(core.Stack):
             cluster=self.base_platform.ecs_cluster,
             cpu=256,
             memory_limit_mib=512,
-            desired_count=self.base_platoform.frontend_desired_count,
+            desired_count=self.base_platform.frontend_desired_count,
             public_load_balancer=True,
             assign_public_ip=True,
             # cloud_map_options=self.base_platform.sd_namespace,
